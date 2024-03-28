061aee330c2994211873a9993213cb1f768d9de7
Andres Osorio <andres.osoriogr@gmail.com>
2024-03-28 13:05:01 -0500

    addition package and testing

--- main/main.go
+++ main/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"onehoax/go_sample/more_strings"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("Hello world!!")
+
+	fmt.Println(more_strings.ReverseRunes(("!oG ,olleH")))
+
+	fmt.Println(cmp.Diff("Hello world", "Hello go"))
 }
